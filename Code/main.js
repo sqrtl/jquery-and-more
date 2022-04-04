876dc7ca5b4a224b090b30bf6d0802b2abb1eec9
Brasil <squirtle.brazil@gmail.com>
2022-04-04 15:28:42 -0400

    first Commit. Challenge 1 and Challenge 2 complete

---
+++ Code/main.js
@@ -0,0 +1,102 @@
+// =================[Body Tags]=============
+const body = $('body');
+
+// =================[JQuery of appending the chart into the div (First Chart)]==============
+const div1 = $('<div></div>');
+const chartCavnas = $('<canvas id="myChart"></canvas>');
+div1.append(chartCavnas);
+body.append(div1);
+
+// ==================[Code for the Sample Graph]==================
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+];
+
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+};
+
+const config = {
+    type: 'line',
+    data: data,
+    options: {}
+};
+
+const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+);
+
+// =================[JQuery of appending the chart into the div (YGO)]==============
+const div2 = div1;
+const ygoCanvas = $('<canvas id="yugioh"></canvas>');
+div2.append(ygoCanvas);
+body.append(div2);
+
+// ==================[Donut chart of OCG MetaBreak down]=====================
+const deckLists = [
+    'Branded Despia',
+    'Rose Dragon Adventurer Tenyi',
+    'Floowandereeze',
+    'Drytron',
+    'Tri-Brigade',
+    'Adventurer Prank-Kids',
+    'Branded Predaplants',
+    'Eldlich',
+    'P.U.N.K. Therions',
+    '@Ignister',
+    'Adamancipator',
+    'Sky Striker',
+    'Tri-Brigade Lyrilusc',
+    'Other'
+];
+
+const ygoData = {
+    label: deckLists,
+    datasets: [{
+        label: 'OCG Metagame Report #9, #10',
+        data: [20, 8, 8, 7, 7, 4, 4, 4, 4, 4, 3, 3, 3, 21],
+        backgroundColor: [
+            'rgb(77,166,255)',
+            'rgb(255,42,0)',
+            'rgb(176,191,26)',
+            'rgb(178,132,190)',
+            'rgb(124,185,232)',
+            'rgb(255,126,0)',
+            'rgb(0,64,128)',
+            'rgb(201,0,22)',
+            'rgb(0,102,0)',
+            'rgb(51,0,102)',
+            'rgb(77,51,0)',
+            'rgb(196,98,16)',
+            'rgb(213,204,255)',
+            'rgb(242,243,244)'
+        ],
+        hoverOffset: 4
+    }]
+};
+
+const ygoConfig = {
+    type: 'doughnut',
+    data: ygoData
+}
+
+const ygoDoughnut = new Chart(
+    document.querySelector('#yugioh'),
+    ygoConfig
+)
+
+
+// console.log(deckLists.length);
+// console.log(ygoData.datasets[0].backgroundColor.length);
